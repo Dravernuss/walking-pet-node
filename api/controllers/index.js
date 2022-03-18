b36a8d4f63ca2f3d47cca1fcb80564e10a70c7d1
LorenaGabbyRojasBello <lorena.rojas@rimac.com.pe>
2022-03-18 15:02:54 -0500

    cambios relacionados a la adaptacion del merge

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,9 +1,8 @@
 import * as walkerCtrl from "./walker.controller.js";
-import * as dateCtrl from "./dates.controller.js";
+import * as dateCtrl from "./date.controller.js";
 import * as userCtrl from "./user.controller.js";
 import * as adminCtrl from "./admin.controller.js";
 import * as petCtrl from "./pet.controller.js";
-import * as dateCtrl from "./date.controller.js";
 import * as commentCtrl from "./comment.controller.js";
 
 export { walkerCtrl, userCtrl, adminCtrl, petCtrl, dateCtrl, commentCtrl };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,8 @@
 import walkerRouter from "./walker.router.js";
-import dateRouter from "./dates.router.js";
+import dateRouter from "./date.router.js";
 import userRouter from "./user.router.js";
 import adminRouter from "./admin.router.js";
 import petRouter from "./pet.router.js";
-import dateRouter from "./date.router.js";
 import commentRouter from "./comment.router.js";
 
 export {
