e1fadc336b628291d4f66e46ffcb431b17cd97a2
mnuxD <mbaellav@uni.pe>
2022-03-30 17:25:28 -0500

    test cypress

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ import {
   dateRouter,
   commentRouter,
 } from "./api/routes/index.js";
-import "dotenv/config";
+// import "dotenv/config";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+// config environments
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
+});
 
 /**
  * Mongoose
